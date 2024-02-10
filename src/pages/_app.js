e698bccca6a7def72fa432d92d5615ecfc872b91
promodp <promod1208@gmail.com>
2024-02-10 20:53:09 +0530

    sketchbook UI

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
 import { Provider } from 'react-redux'
-// import { store } from '@/store'
-// import '@/styles/globals.css'
+import { store } from '../store'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return < >
+  return <Provider store={store}>
     <Component {...pageProps} />
-  </>
+  </Provider>
 }
