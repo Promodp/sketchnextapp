b2fdea4db0be949bd8b068c9662ee829bf77e0f0
promodp <promod1208@gmail.com>
2024-02-10 18:35:32 +0530

    sketchbook app inititialisation using nextjs

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import { Provider } from 'react-redux'
+// import { store } from '@/store'
+// import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return < >
+    <Component {...pageProps} />
+  </>
+}
