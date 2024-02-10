be68e2cef808003abc8874ab78a6d2bb34bee531
promodp <promod1208@gmail.com>
2024-02-10 21:33:19 +0530

    UI pointed to deployed server on render

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000'
+const URL = process.env.NODE_ENV === 'production' ? 'https://sketchbook-server-9zk5.onrender.com/' : 'http://localhost:5000'
 export const socket = io(URL);
